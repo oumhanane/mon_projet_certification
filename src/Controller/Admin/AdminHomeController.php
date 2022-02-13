bb8e1b42cd7de0699c3147d3fe3521164474c1f6
Saliha Ichou <oum.hanane@gmail.com>
2022-02-13 13:38:04 +0100

    creation entity category

---
+++ src/Controller/Admin/AdminHomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminHomeController extends AbstractController
+{
+/**
+ * @route("/admin/home", name= "admin_home")
+ */
+public function home() : Response
+{
+    return $this->render("admin/home.html.twig");
+}
+
+}
